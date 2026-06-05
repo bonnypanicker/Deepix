7585a1e924c9ab123c8c611673321fd47320a7a4
bonnypanicker <bonnypanicker@outlook.com>
2026-06-05 23:25:25 +0530

    Replace Glide RequestListener with OnPreDrawListener for shared-element transition

--- app/src/main/java/com/devomind/gallerysearch/MediaPagerAdapter.kt
+++ app/src/main/java/com/devomind/gallerysearch/MediaPagerAdapter.kt
@@ -1,6 +1,5 @@
 package com.devomind.gallerysearch
 
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,10 +7,6 @@ import android.widget.MediaController
 import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.devomind.gallerysearch.databinding.ItemViewerPageBinding
 
 class MediaPagerAdapter(
@@ -92,32 +87,18 @@ class MediaPagerAdapter(
                     .load(item.uri)
                     .fitCenter()
 
-                if (isInitialSharedElement) {
-                    request.listener(object : RequestListener<Drawable> {
-                        override fun onLoadFailed(
-                            e: GlideException?,
-                            model: Any,
-                            target: Target<Drawable>,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            onInitialImageLoaded()
-                            return false
-                        }
+                request.into(binding.photoView)
 
-                        override fun onResourceReady(
-                            resource: Drawable,
-                            model: Any,
-                            target: Target<Drawable>,
-                            dataSource: DataSource,
-                            isFirstResource: Boolean
-                        ): Boolean {
+                if (isInitialSharedElement) {
+                    val vto = binding.photoView.viewTreeObserver
+                    vto.addOnPreDrawListener(object : android.view.ViewTreeObserver.OnPreDrawListener {
+                        override fun onPreDraw(): Boolean {
+                            vto.removeOnPreDrawListener(this)
                             onInitialImageLoaded()
-                            return false
+                            return true
                         }
                     })
                 }
-
-                request.into(binding.photoView)
             }
         }
 
